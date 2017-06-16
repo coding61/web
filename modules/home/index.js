a0df61de3c3c35d6139c687cf9bd66e99467cc2a
chen <chen158_7@126.com>
2017-06-16 18:23:43 +0800

    debug

--- modules/home/index.js
+++ modules/home/index.js
@@ -5,11 +5,25 @@ define(function(require, exports, module) {
     var Page = {
         token:null,
         init:function(){
+            
+            Team.init();
 
             Page.clickEvent();
         },
         load:function(){
             
+        },
+        authLogin:function(url){
+            // 先微信授权登录
+            // 微信网页授权
+            var appId = 'wx58e15a667d09d70f',
+                redirectUri = "https://www.cxy61.com/mobile/html/wechatHB.html?v=1.0.7",
+                scope = 'snsapi_userinfo';
+
+            redirectUri = url;
+            redirectUri = encodeURIComponent(redirectUri);
+
+            location.href="https://open.weixin.qq.com/connect/oauth2/authorize?appid="+appId+"&redirect_uri="+redirectUri+"&response_type=code&scope="+scope+"&state=STATE#wechat_redirect"
         },
         clickEvent:function(){
             
@@ -30,14 +44,8 @@ define(function(require, exports, module) {
                     }else{
                         // 先微信授权登录
                         // 微信网页授权
-                        var appId = 'wx58e15a667d09d70f',
-                            redirectUri = "https://www.cxy61.com/mobile/html/wechatHB.html?v=1.0.7",
-                            scope = 'snsapi_userinfo';
-
-                        redirectUri = 'https://www.cxy61.com/cxyteam/app/home/createTeam.html';
-                        redirectUri = encodeURIComponent(redirectUri);
-
-                        location.href="https://open.weixin.qq.com/connect/oauth2/authorize?appid="+appId+"&redirect_uri="+redirectUri+"&response_type=code&scope="+scope+"&state=STATE#wechat_redirect"
+                        var redirectUri = 'https://www.cxy61.com/cxyteam/app/home/createTeam.html';
+                        Page.authLogin(redirectUri);
                     }
                 })
             });
@@ -46,6 +54,11 @@ define(function(require, exports, module) {
             $(".join").click(function(){
                 // 随机匹配进入我的团队页
                 $(".wait-loading").show();
+
+                // 先微信授权登录
+                // 微信网页授权
+                var redirectUri = 'https://www.cxy61.com/cxyteam/app/home/index.html';
+                Page.authLogin(redirectUri);
             })
             
             $(".my").click(function(){
@@ -56,20 +69,78 @@ define(function(require, exports, module) {
                     }else{
                         // 先微信授权登录
                         // 微信网页授权
-                        var appId = 'wx58e15a667d09d70f',
-                            redirectUri = "https://www.cxy61.com/mobile/html/wechatHB.html?v=1.0.7",
-                            scope = 'snsapi_userinfo';
-
-                        redirectUri = 'https://www.cxy61.com/cxyteam/app/home/createTeam.html';
-                        redirectUri = encodeURIComponent(redirectUri);
-
-                        location.href="https://open.weixin.qq.com/connect/oauth2/authorize?appid="+appId+"&redirect_uri="+redirectUri+"&response_type=code&scope="+scope+"&state=STATE#wechat_redirect"
+                        var redirectUri = 'https://www.cxy61.com/cxyteam/app/home/myTeam.html';
+                        Page.authLogin(redirectUri);
                     }
                 })                
             });
         }
     };
+    
+    var Team = {
+        code:Common.getQueryString('code'),
+        init:function(){
 
+            if (Team.code) {
+                $(".wait-loading").show();
+                Team.getToken();
+            }
+        },
+        getToken:function(){
+            $.ajax({
+                type:'post',
+                url:Common.domain + "/userinfo/code_login/",
+                data:{
+                    code:Team.code
+                },
+                success:function(json){
+                    if(window.localStorage){
+                        localStorage.token = json.token;
+                    }else{
+                        $.cookie("token", json.token, {
+                            path: "/"
+                        });
+                    }
+                    Team.joinUnknownTeam();
+                },
+                error:function(xhr, textStatus){
+                    if (textStatus == "timeout") {
+                        Common.showToast("服务器开小差了");
+                    }
+                    Common.dialog(JSON.parse(xhr.responseText).message||JSON.parse(xhr.responseText).detail);
+                    console.log(textStatus);
+                }
+            })
+        },
+        joinUnknownTeam:function(){
+            Common.isLogin(function(token){
+                if (token == "null") {
+                    Common.dialog("请先授权");
+                    return;
+                }
+                $.ajax({
+                    type:"get",
+                    url:Common.domain + "/userinfo/random_join_group/",
+                    headers:{
+                        Authorization:"Token " + token
+                    },
+                    success:function(json){
+
+                       location.href = "myTeam.html";
+                    },
+                    error:function(xhr, textStatus){
+                        if (textStatus == "timeout") {
+                            Common.showToast("服务器开小差了");
+                            return;
+                        }
+                        Common.dialog(JSON.parse(xhr.responseText).message||JSON.parse(xhr.responseText).detail);
+                        console.log(textStatus);
+                    }
+                })
+            })
+        },
+        
+    }
     Page.init();
 
 });
--- modules/home/myTeam.js
+++ modules/home/myTeam.js
@@ -165,15 +165,6 @@ define(function(require, exports, module) {
 
         },
         clickEvent:function(){
-            var appId = 'wx58e15a667d09d70f',
-                redirectUri = "https://www.cxy61.com/mobile/html/wechatHB.html?v=1.0.7",
-                scope = 'snsapi_userinfo';
-
-            redirectUri = encodeURIComponent(redirectUri);
-
-            $(".btn-wx-auth").click(function(){
-                location.href="https://open.weixin.qq.com/connect/oauth2/authorize?appid="+appId+"&redirect_uri="+encodeURI(redirectUri)+"&response_type=code&scope="+scope+"&state=STATE#wechat_redirect"
-            })
 
             // textarea编辑框高度自适应
              $('.main-view .team .intro textarea').on('input', function(){
@@ -248,43 +239,69 @@ define(function(require, exports, module) {
                     // 邀请分享
                     $(".shadow-view").show();
 
+                    var url = window.location.href;
+                    if (url.indexOf("pk") != -1) {
+                        
+                    }else{
+                        location.href = url + "?pk=" + Team.pk;
+                    }
+
                 }else if ($(this).hasClass('join')) {
                     // 申请加入
-                    Common.confirm("您确定要加入此战队吗?", function(){
+                    if (Team.pk && !Team.code) {
+                        // 分享进来的页面, 点加入要先授权
+                        // 微信网页授权
+                        var appId = 'wx58e15a667d09d70f',
+                            redirectUri = "https://www.cxy61.com/mobile/html/wechatHB.html?v=1.0.7",
+                            scope = 'snsapi_userinfo';
 
-                        // 加入团队
-                        Team.joinTeam($(this));
+                        redirectUri = 'https://www.cxy61.com/cxyteam/app/home/myTeam.html';
+                        redirectUri = encodeURIComponent(redirectUri);
 
-                        /*
-                        var parent =  $(".default-avatar").eq(0).parent();
-                        parent.children('.default-avatar').children('img').attr({src:'../../statics/images/11.jpg'});
-                        parent.children('.default-avatar').removeClass("default-avatar").addClass('avatar');
-                        parent.children('.name').html("张三美");
+                        location.href="https://open.weixin.qq.com/connect/oauth2/authorize?appid="+appId+"&redirect_uri="+redirectUri+"&response_type=code&scope="+scope+"&state=STATE#wechat_redirect"
+                    }else{
+                        Common.confirm("您确定要加入此战队吗?", function(){
 
-                        // 当前用户加入后, 变邀请
-                        $(this).children('span').html('邀请好友加入');
-                        $(this).removeClass('join').addClass('share');
-                        */
+                            // 加入团队
+                            Team.joinTeam($(this));
 
-                    })
+                            /*
+                            var parent =  $(".default-avatar").eq(0).parent();
+                            parent.children('.default-avatar').children('img').attr({src:'../../statics/images/11.jpg'});
+                            parent.children('.default-avatar').removeClass("default-avatar").addClass('avatar');
+                            parent.children('.name').html("张三美");
+
+                            // 当前用户加入后, 变邀请
+                            $(this).children('span').html('邀请好友加入');
+                            $(this).removeClass('join').addClass('share');
+                            */
+
+                        })
+                    }
+                    
                     
                 }else if ($(this).hasClass('unjoin')) {
                     // 不能加入
-                    
-                    Common.dialog('网络失败');
-                    
+                                        
                 }
             })
         }
     };
 
     var Team = {
-        pk:null,
+        pk:Common.getQueryString("pk"),
         currentUser:null,  //当前用户的 pk
         data:null, //小组数据
         code:Common.getQueryString('code'),
         init:function(){
-            if (Team.code) {
+            if (Team.code && !Team.pk) {
+                // 上一页进来的
+                Team.getToken();
+            }else if(Team.pk && !Team.code){
+                // 从分享进来,加载 team 信息
+                Team.loadShareTeam();
+            }else if(Team.pk && Team.code){
+                // 分享进来的先授权,在加载team信息
                 Team.getToken();
             }else{
                 Team.loadInfo();
@@ -305,7 +322,7 @@ define(function(require, exports, module) {
                             path: "/"
                         });
                     }
-                    
+
                     Team.loadInfo();
                 },
                 error:function(xhr, textStatus){
@@ -331,8 +348,36 @@ define(function(require, exports, module) {
                     },
                     success:function(json){
                         Team.currentUser = json.pk;
-
-                        Team.load();
+                        
+                        if (Team.pk && Team.code) {
+                            Team.loadShareTeam();   
+                        }else{
+                            Team.load();
+                        }
+                        
+                    },
+                    error:function(xhr, textStatus){
+                        if (textStatus == "timeout") {
+                            Common.showToast("服务器开小差了");
+                            return;
+                        }
+                        Common.dialog(JSON.parse(xhr.responseText).message||JSON.parse(xhr.responseText).detail);
+                        console.log(textStatus);
+                    }
+                })
+            })
+        },
+        loadShareTeam:function(){
+            Common.isLogin(function(token){
+                if (token == "null") {
+                    Common.dialog('请先授权');
+                    return;
+                }
+                $.ajax({
+                    type:"get",
+                    url:Common.domain + "/userinfo/groups/"+Team.pk+"/",
+                    success:function(json){
+                        Team.adjustData(json);
                     },
                     error:function(xhr, textStatus){
                         if (textStatus == "timeout") {
@@ -358,79 +403,8 @@ define(function(require, exports, module) {
                         Authorization:"Token " + token
                     },
                     success:function(json){
-                        console.log(json);
-
-                        Team.pk = json.pk;
-                        
-                        // leader:true,   //当前用户是组长吗
-                        // inTeam:true,  //当前用户是否在团队里,
-                        // isManyuan:true,  //团队是否满员
-                        var leader=false,
-                            inTeam=false, 
-                            isManyuan=false;
-                        if (json.member_number == 4) {
-                            isManyuan = true;
-                        }else{
-                            isManyuan = false;
-                        }
-                        
-                        var members = json.group_member;
-                        for (var i = 0; i < members.length; i++) {
-                            if (members[i].owner.pk == Team.currentUser){
-                                inTeam = true;
-                                break;
-                            }
-                        }
-                        for (var i = 0; i < members.length; i++) {
-                            if(members[i].leader=true){
-                                if (members[i].owner.pk == Team.currentUser) {
-                                    //当前用户是组长
-                                    leader = true;
-                                }else{
-                                    leader = false;
-                                }
-
-                                break;
-                            }
-                        }
-
-                        var dic = json;
-
-                        dic['leader'] = leader;
-                        dic['inTeam'] = inTeam;
-                        dic['isManyuan'] = isManyuan;
-
-                        
-                        // 将队长放到最前面
-                        var number = null,
-                            targent = null;
-                        for (var i = 0; i < dic.group_member.length; i++) {
-                            if(dic.group_member[i].leader == true){
-                                number = i;
-                                targent = dic.group_member[i];
-                                break;
-                            }
-                        }
-                        if (number != null && targent != null) {
-                            dic.group_member.splice(number,1);
-                            dic.group_member.unshift(targent);  //将全部相册放到第一位
-                        }else{
-                        }
-                        
-                        console.log(dic);
-                        var html = ArtTemplate("main-view-template", dic);
-                        $(".body-view").html(html);
-
-                        if (dic.inTeam == true && dic.leader == true) {
-                            $(".leader").show();
-
-                            $(".leader.editing").hide();  //关闭点编辑出来的元素
-                        }else{
-                            $(".leader").hide();
-                        }
-
-                        Page.clickEvent();
-
+                        // console.log(json);
+                        Team.adjustData(json);
                     },
                         
                     error:function(xhr, textStatus){
@@ -443,6 +417,78 @@ define(function(require, exports, module) {
                     }
                 })
             })
+        },
+        adjustData:function(json){
+            Team.pk = json.pk;
+                        
+            // leader:true,   //当前用户是组长吗
+            // inTeam:true,  //当前用户是否在团队里,
+            // isManyuan:true,  //团队是否满员
+            var leader=false,
+                inTeam=false, 
+                isManyuan=false;
+            if (json.member_number == 4) {
+                isManyuan = true;
+            }else{
+                isManyuan = false;
+            }
+            
+            var members = json.group_member;
+            for (var i = 0; i < members.length; i++) {
+                if (members[i].owner.pk == Team.currentUser){
+                    inTeam = true;
+                    break;
+                }
+            }
+            for (var i = 0; i < members.length; i++) {
+                if(members[i].leader=true){
+                    if (members[i].owner.pk == Team.currentUser) {
+                        //当前用户是组长
+                        leader = true;
+                    }else{
+                        leader = false;
+                    }
+
+                    break;
+                }
+            }
+
+            var dic = json;
+
+            dic['leader'] = leader;
+            dic['inTeam'] = inTeam;
+            dic['isManyuan'] = isManyuan;
+
+            
+            // 将队长放到最前面
+            var number = null,
+                targent = null;
+            for (var i = 0; i < dic.group_member.length; i++) {
+                if(dic.group_member[i].leader == true){
+                    number = i;
+                    targent = dic.group_member[i];
+                    break;
+                }
+            }
+            if (number != null && targent != null) {
+                dic.group_member.splice(number,1);
+                dic.group_member.unshift(targent);  //将全部相册放到第一位
+            }else{
+            }
+            
+            console.log(dic);
+            var html = ArtTemplate("main-view-template", dic);
+            $(".body-view").html(html);
+
+            if (dic.inTeam == true && dic.leader == true) {
+                $(".leader").show();
+
+                $(".leader.editing").hide();  //关闭点编辑出来的元素
+            }else{
+                $(".leader").hide();
+            }
+
+            Page.clickEvent();
         },
         updateTeam:function(name, intro){
             Common.isLogin(function(token){
