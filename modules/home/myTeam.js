63aeedf603cafa1843f21539bed340ec9049325b
chen <chen158_7@126.com>
2017-06-17 20:03:50 +0800

    debug

--- modules/home/myTeam.js
+++ modules/home/myTeam.js
@@ -302,7 +302,10 @@ define(function(require, exports, module) {
                             Common.showToast("服务器开小差了");
                             return;
                         }
-                        if (xhr.status == 400 || xhr.status == 403) {
+                        if (xhr.status == 404) {
+                            Common.showToast("此团队已解散");
+                            return;
+                        }else if (xhr.status == 400 || xhr.status == 403) {
                             Common.dialog(JSON.parse(xhr.responseText).message||JSON.parse(xhr.responseText).detail);
                             return;
                         }else if (xhr.status == 401) {
