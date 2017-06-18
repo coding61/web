46703097efe4befc4f07fcd848409e08b0d99f69
chen <chen158_7@126.com>
2017-06-18 12:11:50 +0800

    debug

--- modules/common/WXJSSDK.js
+++ modules/common/WXJSSDK.js
@@ -1,8 +1,4 @@
-define(function(require, exports, module) {
-	var ArtTemplate = require("libs/template.js");
-	var Common = require('common/common.js');
-	var AppBridge = require("common/app-bridge.js");
-	
+define(function(require, exports, module) {	
 	// exports.config = function(){
 	// 	wx.config({
 	// 	    debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
