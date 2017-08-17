73392030b38a3ea600a9b845339f7a9815cd2e07
chen <chen158_7@126.com>
2017-08-17 18:44:17 +0800

    debug codeCompile

--- modules/home/codeCompileRN.js
+++ modules/home/codeCompileRN.js
@@ -54,7 +54,7 @@ define(function(require, exports, module) {
             document.addEventListener('message', function(e) {  
                 var a = e.data;   
                 console.log(a);
-                // alert(a);
+                alert(a);
 
                 htmlEditor.setOption("mode", editModes[a].mode);
                 htmlEditor.setValue("");
