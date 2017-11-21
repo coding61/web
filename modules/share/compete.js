c1a77488d8a2f0e0b3ddcde6a3163c66c504ea72
JackieQu <13263699826@163.com>
2017-11-21 21:04:38 +0800

    竞赛分享

--- modules/share/compete.js
+++ modules/share/compete.js
@@ -8,7 +8,8 @@ define(function(require, exports, module) {
 			$('.download').click(function() {
 				Page.download();
 			})
-            Page.loadClubDetails(pk);
+            // Page.loadClubDetails(pk);
+            Page.loadCompeteDetails(pk)
         },
 
         // 获取活动详情
@@ -44,6 +45,36 @@ define(function(require, exports, module) {
             })
         },
 
+        loadCompeteDetails:function(pk) {
+            $.ajax({
+                type: "get",
+                url: Common.domain + "/contest/" + pk + "/question",
+                headers: {
+                },
+                success:function(json){
+                    if (json.results.length == 0) {
+                        alert('该竞赛下暂无题目');
+                    } else {
+                        // 可能有多个题目，但目前还未设计对应 UI，先展示一个题目
+                        var dataList = [];
+                        dataList.push(json.results[0]);
+                        var html = template('details-template', dataList);
+                        $('.details').html(html);
+                        $('.answer').click(function() {
+    						Page.download();
+    					})
+                        
+                        var question = null;
+                        question = json.results[0].title.replace(/\r\n/g,"<br>");
+    					question = json.results[0].title.replace(/\n/g,"</br>");
+                        $('.item-content').html(question);
+                    }
+                },
+                error:function(xhr, textStatus){
+                    Page.exceptionHandling(xhr, textStatus);
+                }
+            })
+        },
 
         // 异常处理
         exceptionHandling:function(xhr, textStatus) {
