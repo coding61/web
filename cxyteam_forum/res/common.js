de5dd6b8271310dd7f1de964a1a4b6e58cddab3c
姚艳宇 <yaoyanyu@yaoyanyudeAir.modouwifi.com>
2017-11-23 00:16:51 +0800

    融云本地存储用户信息

--- cxyteam_forum/res/common.js
+++ cxyteam_forum/res/common.js
@@ -6,12 +6,12 @@ window.RongDemo = {
     common: function (WebIMWidget, config, $scope) {
         WebIMWidget.init(config);
         var basePath="/program_girl";
-        var userlist = []; //用来存用户信息
-
-        WebIMWidget.setUserInfoProvider(function(targetId, obj) {
-            if (userlist.length != 0) {
+        
+        WebIMWidget.setUserInfoProvider(function(targetId, obj) { 
+            if (localStorage.userlist) {
+                var userlist = JSON.parse(localStorage.userlist);
                 var user;
-                userlist.forEach(function(item) {
+                userlist.result.forEach(function(item) {
                     if (item.id == targetId) {
                         user = item;
                     }
@@ -21,24 +21,30 @@ window.RongDemo = {
                 } else {
                     // 根据id获取用户信息
                     $.ajax({
+                        async: false,
                         url: basePath + "/userinfo/username_userinfo/?username=" + targetId,
                     }).success(function(rep){
                         // console.log(rep);
                         var userJson = {"id": targetId,"name": rep.name, "portraitUri": rep.avatar};
-                        userlist.push(userJson); //存用户信息
+                        userlist.result.push(userJson);
+                        localStorage.userlist = JSON.stringify(userlist);
                         obj.onSuccess({name: rep.name, id: targetId, portraitUri: rep.avatar});
                     }).error(function(err) {
 
                     })
                 }
             } else {
+                var userlist = {"result":[]};
                 // 根据id获取用户信息
                 $.ajax({
+                    async: false,
                     url: basePath + "/userinfo/username_userinfo/?username=" + targetId,
                 }).success(function(rep){
                     // console.log(rep);
+                    
                     var userJson = {"id": targetId,"name": rep.name, "portraitUri": rep.avatar};
-                    userlist.push(userJson); //存用户信息
+                    userlist.result.push(userJson); //存用户信息
+                    localStorage.userlist = JSON.stringify(userlist);
                     obj.onSuccess({name: rep.name, id: targetId, portraitUri: rep.avatar});
                 }).error(function(err) {
 
@@ -48,9 +54,10 @@ window.RongDemo = {
         });
 
         WebIMWidget.setGroupInfoProvider(function(targetId, obj){
-            if (userlist.length != 0) {
+            if (localStorage.userlist) {
+                var userlist = JSON.parse(localStorage.userlist);
                 var user;
-                userlist.forEach(function(item) {
+                userlist.result.forEach(function(item) {
                     if (item.id == targetId) {
                         user = item;
                     }
@@ -59,6 +66,7 @@ window.RongDemo = {
                     obj.onSuccess({name: user.name, id: user.id});
                 } else {
                     $.ajax({
+                        async: false,
                         url: basePath + "/club/club_detail/" + targetId + "/",
                         headers: {
                             'Authorization': "Token " + localStorage.token
@@ -66,22 +74,27 @@ window.RongDemo = {
                     }).success(function(rep){
                         // console.log(rep);
                         var userJson = {"id": targetId,"name": rep.name};
-                        userlist.push(userJson); //存用户信息
+                        userlist.result.push(userJson);
+                        localStorage.userlist = JSON.stringify(userlist);
                         obj.onSuccess({id: targetId, name: rep.name});
                     }).error(function(err) {
 
                     })
                 }
             } else {
+                var userlist = {"result":[]};
                 $.ajax({
+                    async: false,
                     url: basePath + "/club/club_detail/" + targetId + "/",
                     headers: {
                         'Authorization': "Token " + localStorage.token
                     }
                 }).success(function(rep){
                     // console.log(rep);
+                    
                     var userJson = {"id": targetId,"name": rep.name};
-                    userlist.push(userJson); //存用户信息
+                    userlist.result.push(userJson); //存用户信息
+                    localStorage.userlist = JSON.stringify(userlist);
                     obj.onSuccess({id: targetId, name: rep.name});
                 }).error(function(err) {
 
