0052a73c86711b5a358fff8e6fd491bdf116333e
chen <chen158_7@126.com>
2018-09-12 17:52:34 +0800

    de

--- modules/team/wxshare.js
+++ modules/team/wxshare.js
@@ -56,6 +56,7 @@ define(function(require, exports, module) {
                     // 用户点击了分享后执行的回调函数
                 }
             });
+            
             // 朋友(即将废弃)
             wx.onMenuShareAppMessage({
                 title: title,        // 分享标题
@@ -68,6 +69,7 @@ define(function(require, exports, module) {
                     // 用户点击了分享后执行的回调函数
                 }
             });
+        });
 
         wx.error(function(res) {
             // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
