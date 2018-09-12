b6f54f793c574f7e3cd80524ee6e8bccfdf4ca28
chen <chen158_7@126.com>
2018-09-12 18:02:49 +0800

    de

--- modules/team/wxshare.js
+++ modules/team/wxshare.js
@@ -19,7 +19,7 @@ define(function(require, exports, module) {
     }
     function ConfigShare(json, title, desc, url, imgUrl){
         wx.config({
-          debug: true,                    // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+          debug: false,                    // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
           appId: json.appid,               // 必填，公众号的唯一标识
           timestamp: json.timestamp,       // 必填，生成签名的时间戳
           nonceStr: json.nonceStr,         // 必填，生成签名的随机串
@@ -77,7 +77,7 @@ define(function(require, exports, module) {
             // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
             console.log('weixin 验证失败');
             console.log(res);
-            Common.dialog(res);
+            // Common.dialog(res);
         });
     }
 
